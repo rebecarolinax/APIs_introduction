0fd0cd05c24aa46e1431164e32d59c6569b1f827
Rebeca Carolina <@rebecacarolina75@gmail.com>
2023-09-01 17:14:40 -0300

    Web API's

---
+++ exercício-filmes/webapi.Filmes/Program.cs
@@ -0,0 +1,56 @@
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+// Adiciona o serviço de controllers
+builder.Services.AddControllers();
+
+//Adiciona serviço de autenticação(JWT Bearer)
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultChallengeScheme = "JwtBearer";
+    options.DefaultAuthenticateScheme = "JwtBearer";
+})
+
+//Define os parâmetros de validação do token
+.AddJwtBearer(options =>{}); //paramos aqui...continuar na segunda
+
+
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Filmes API",
+        Description = "API para cadastrar gêneros e filmes",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Rebeca Carolina GitHub",
+            Url = new Uri("https://github.com/rebecarolinax")
+        }
+    });
+
+    // using System.Reflection;
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
+
+// Mapear os controllers
+app.MapControllers();
+app.Run();
