ce9d6ad25040b97af05b7749a2d3fc4260b054a2
Rebeca Carolina Fantini Furini De Lima <54931473865@info.local>
2023-10-31 14:55:31 -0300

    :bulb: Web API's

--- exercício-filmes/webapi.Filmes/Interfaces/IUsuarioRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using webapi.filmes.tarde.Domains;
-
-namespace webapi.filmes.tarde.Interfaces
-{
-    public interface IUsuarioRepository
-    {
-        UsuarioDomain Login(string email, string senha);
-    }
-}
